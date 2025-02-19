1f47d4331f1c963a7e14d55d7dd12a195edf5a98
Tom Fielder <tom.fielder97@gmail.com>
2025-02-19 00:34:24 +0000

    Add vercel analytics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "@/app/components/Hero";
 import About from "./components/About";
 import Contact from "./components/Contact";
-import Skills from "./components/Skills/page";
+import Skills from "./components/Skills";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function Home() {
 	return (
 		<>
+			<Analytics />
 			<Hero />
 			<div className="h-screen flex flex-col gap-12 md:justify-evenly">
 				<About />
